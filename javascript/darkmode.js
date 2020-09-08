26bf345c2222e2673956a801b052723150b38995
camilo ortiz <camilodortiz@gmail.com>
2020-09-08 14:08:28 -0500

    remove logs

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,5 +1,3 @@
 let now = new Date().getHours();
-console.log(now);
 if (now > 20 || now < 7) document.body.style.backgroundColor = '#ffeac3'; //#c3d8ff
 else document.body.style.backgroundColor = '#fffcf6';
-console.log('update');

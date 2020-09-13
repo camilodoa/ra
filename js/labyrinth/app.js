a4148d7a9af179cf3c5db448df52918623b75616
camilo ortiz <camilodortiz@gmail.com>
2020-09-13 17:19:00 -0500

    remove labyrinth in master

--- js/labyrinth/app.js
+++
@@ -1,7 +0,0 @@
-class labyrinth {
-    constructor (width, height, baseX, baseY){
-        this.map = this.makeMap(width, height);
-        this.walls = this.makeWalls(this.map);
-        this.labyrinth = this.makeLabyrinth(this.map, this.walls);
-    }
-}

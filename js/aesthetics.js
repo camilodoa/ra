16e049835bde077962aeb1a54034c688cce8c552
camilodoa <camilodortiz@gmail.com>
2022-04-16 01:18:12 -0500

    Remove border and add teleport from goal

--- js/aesthetics.js
+++
@@ -1,8 +0,0 @@
-// border color
-let now = new Date().getHours();
-if (now > 20 || now < 7) {
-  document.body.style.backgroundColor = '#ffeac3'; //#c3d8ff
-}
-else {
-  document.body.style.backgroundColor = '#fffcf6';
-}
